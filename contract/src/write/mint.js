0fbe016cf32a3b92b6840c21a12c1510fdb2e31c
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-18 06:08:53 -0500

    fix: move util Contract Error func

--- contract/src/write/mint.js
+++ contract/src/write/mint.js
@@ -56,14 +56,3 @@ export const mint = (state, action) => {
       }
     );
 };
-
-/**
- * @description Same as ContractAssert with a passthrough
- *
- * @author @jshaw-ar
- * @param {*} flag What your conditional check is
- * @param {*} message Error message if conditional is true
- * @param {*} p The payload to pass through the func
- * @return {*} p
- */
-const ca = (flag, message) => (p) => flag ? Left(message) : Right(p);
