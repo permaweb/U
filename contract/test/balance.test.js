09053de6eed8a9747a5e9ce296ee8ebd0932ad84
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-18 07:08:41 -0500

    test: balance 0 for caller and with target

---
+++ contract/test/balance.test.js
@@ -0,0 +1,58 @@
+import { suite } from "uvu";
+import * as assert from "uvu/assert";
+import { balance } from "../src/read/balance.js";
+import { setupSmartWeaveEnv } from "./setup.js";
+const test = suite("balance");
+
+test.before(async () => {});
+
+test("caller should have 0 balance", async () => {
+  // set reward to 10
+  setupSmartWeaveEnv(999999);
+
+  const caller = "<justin>";
+  const output = await balance(
+    {
+      name: "rebar",
+      ticker: "rebar",
+      balances: {},
+      settings: [
+        ["communityLogo", "_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo"],
+        ["isTradeable", true],
+      ],
+      claims: [],
+      claimable: [],
+      divisibility: 6,
+    },
+    { caller }
+  );
+  assert.is(output.result.balance, 0);
+});
+
+test("target should have 0 balance", async () => {
+  // set reward to 10
+  setupSmartWeaveEnv(999999);
+
+  const caller = "<justin>";
+  const output = await balance(
+    {
+      name: "rebar",
+      ticker: "rebar",
+      balances: {},
+      settings: [
+        ["communityLogo", "_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo"],
+        ["isTradeable", true],
+      ],
+      claims: [],
+      claimable: [],
+      divisibility: 6,
+    },
+    { caller, input: { target: "<tom>" } }
+  );
+  assert.is(output.result.balance, 0);
+  assert.is(output.result.target, "<tom>");
+});
+
+test.after(async () => {});
+
+test.run();
