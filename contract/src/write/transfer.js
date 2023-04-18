047b7fb491fc9427063b9df17b3743d3bb8e568c
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-18 10:50:31 -0500

    feat: transfer with tests

--- contract/src/write/transfer.js
+++ contract/src/write/transfer.js
@@ -1,18 +1,40 @@
+import { fromNullable, of } from "../hyper-either.js";
+import { ce } from "../util.js";
+
 export function transfer(state, action) {
-  const { balances } = state;
-  const { caller, input } = action;
-  const { qty, target } = input;
-  validateQuantityGreaterThanZero(qty);
-
-  validateTarget(caller, target);
-
-  validateBalanceGreaterThanQuantity(balances[caller], qty);
-  balances[caller] -= qty;
-  if (!balances[target]) {
-    balances[target] = 0;
-  }
-  validateBalance(balances[caller]);
-  validateBalance(balances[target]);
-  balances[target] += qty;
-  return { state };
+  return of({ state, action })
+    .chain(fromNullable)
+    .chain(ce(!action.input?.target, "Please specify a target."))
+    .chain(
+      ce(action.input?.target === action.caller, "Target cannot be caller.")
+    )
+    .chain(
+      ce(
+        !Number.isInteger(state.balances[action.caller]),
+        "Caller does not have a balance."
+      )
+    )
+    .chain(ce(!Number.isInteger(action.input?.qty), "qty must be an integer."))
+    .chain(
+      ce(
+        state.balances[action.caller] < action.input?.qty,
+        "Not enough tokens for transfer."
+      )
+    )
+    .fold(
+      (msg) => {
+        throw new ContractError(msg || "An error occurred.");
+      },
+      ({ state, action }) => {
+        const { input, caller } = action;
+        const { target, qty } = input;
+        const { balances } = state;
+        balances[caller] -= qty;
+        if (!balances[target]) {
+          balances[target] = 0;
+        }
+        balances[target] += qty;
+        return state;
+      }
+    );
 }
