ae72ef2981f8fc6362ab4945f29344cb4d847e0d
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-18 10:02:31 -0500

    feat: claim

--- contract/src/write/claim.js
+++ contract/src/write/claim.js
@@ -1,21 +1,67 @@
+import { fromNullable, of } from "../hyper-either.js";
+import { ce } from "../util.js";
+
+/**
+ * Claims rebAR from claimables
+ *
+ * @author @jshaw-ar
+ * @export
+ * @param {*} state
+ * @param {*} action
+ * @return {*}
+ */
 export function claim(state, action) {
-  const { claimable, claims, balances } = state;
-  const { caller, input } = action;
-  const { qty, tx } = input;
-  const obj = validateClaim(tx, claims, claimable, caller);
-
-  validateTx(tx);
-
-  const filteredClaim = claimable.filter((c) => c.tx === tx)[0];
-  const filteredClaimable = claimable.filter((c) => c.tx !== tx);
-
-  validateQuantityOfClaim(qty, filteredClaim.qty);
-
-  if (!Number.isInteger(balances[caller])) balances[caller] = 0;
-
-  balances[caller] += obj.qty;
-  claims.push(tx);
-  state.claimable = filteredClaimable;
-
-  return { state };
+  return of({ state, action })
+    .chain(fromNullable)
+    .chain(
+      ce(!action.input?.txID, "txID must be passed to the claim function.")
+    )
+    .chain(
+      ce(state.claims.includes(action.input?.txID), "Claim already processed.")
+    )
+    .chain(
+      ce(
+        !(
+          state.claimable.filter((c) => c.txID === action.input.txID).length ===
+          1
+        ),
+        "There must be 1 claimable with this tx id."
+      )
+    )
+    .chain(
+      ce(
+        state.claimable.filter((c) => c.txID === action.input.txID)[0]?.to !==
+          action.caller,
+        "Claim not addressed to caller."
+      )
+    )
+    .chain(
+      ce(
+        !(
+          action.input?.qty ===
+          state.claimable.filter((c) => c.txID === action.input.txID)[0]?.qty
+        ),
+        "Incorrect qty."
+      )
+    )
+    .fold(
+      (msg) => {
+        throw new ContractError(msg || "An error occurred.");
+      },
+      ({ state, action }) => {
+        const { balances, claimable, claims } = state;
+        const { input, caller } = action;
+        const { txID } = input;
+        if (!Number.isInteger(balances[caller])) balances[caller] = 0;
+        const claim = claimable.filter((c) => c.txID === txID)[0];
+        balances[caller] += claim.qty;
+        claims.push(claim.txID);
+        return {
+          state: {
+            ...state,
+            claimable: claimable.filter((c) => c.txID !== claim.txID),
+          },
+        };
+      }
+    );
 }
---
+++ contract/test/claim.test.js
@@ -0,0 +1,173 @@
+import { suite } from "uvu";
+import * as assert from "uvu/assert";
+import { claim } from "../src/write/claim.js";
+import { setupSmartWeaveEnv } from "./setup.js";
+const test = suite("claim");
+
+test.before(async () => {});
+
+test("should throw (txID must be passed to the claim function.)", () => {
+  setupSmartWeaveEnv();
+  const caller = "<justin>";
+  assert.throws(
+    () =>
+      claim(
+        {
+          name: "rebar",
+          ticker: "rebar",
+          balances: {},
+          settings: [
+            ["communityLogo", "_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo"],
+            ["isTradeable", true],
+          ],
+          claims: [],
+          claimable: [],
+          divisibility: 6,
+        },
+        { caller }
+      ),
+    /txID must be passed to the claim function./
+  );
+});
+
+test("should throw (Claim already processed.)", () => {
+  setupSmartWeaveEnv();
+  const caller = "<justin>";
+  assert.throws(
+    () =>
+      claim(
+        {
+          name: "rebar",
+          ticker: "rebar",
+          balances: {},
+          settings: [
+            ["communityLogo", "_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo"],
+            ["isTradeable", true],
+          ],
+          claims: ["<test-claim>"],
+          claimable: [],
+          divisibility: 6,
+        },
+        { caller, input: { txID: "<test-claim>" } }
+      ),
+    /Claim already processed./
+  );
+});
+
+test("should throw (There must be 1 claimable with this tx id.)", () => {
+  setupSmartWeaveEnv();
+  const caller = "<justin>";
+  assert.throws(
+    () =>
+      claim(
+        {
+          name: "rebar",
+          ticker: "rebar",
+          balances: {},
+          settings: [
+            ["communityLogo", "_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo"],
+            ["isTradeable", true],
+          ],
+          claims: [],
+          claimable: [],
+          divisibility: 6,
+        },
+        { caller, input: { txID: "<test-claim>" } }
+      ),
+    /There must be 1 claimable with this tx id./
+  );
+});
+
+test("should throw (Claim not addressed to caller.)", () => {
+  setupSmartWeaveEnv();
+  const caller = "<justin>";
+  assert.throws(
+    () =>
+      claim(
+        {
+          name: "rebar",
+          ticker: "rebar",
+          balances: {},
+          settings: [
+            ["communityLogo", "_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo"],
+            ["isTradeable", true],
+          ],
+          claims: [],
+          claimable: [
+            {
+              txID: "<test-claim>",
+              to: "<tom>",
+            },
+          ],
+          divisibility: 6,
+        },
+        { caller, input: { txID: "<test-claim>" } }
+      ),
+    /Claim not addressed to caller./
+  );
+});
+
+test("should throw (Incorrect qty.)", () => {
+  setupSmartWeaveEnv();
+  const caller = "<justin>";
+  assert.throws(
+    () =>
+      claim(
+        {
+          name: "rebar",
+          ticker: "rebar",
+          balances: {},
+          settings: [
+            ["communityLogo", "_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo"],
+            ["isTradeable", true],
+          ],
+          claims: [],
+          claimable: [
+            {
+              txID: "<test-claim>",
+              to: caller,
+              qty: 11,
+            },
+          ],
+          divisibility: 6,
+        },
+        { caller, input: { txID: "<test-claim>", qty: 10 } }
+      ),
+    /Incorrect qty./
+  );
+});
+
+test("should claim", () => {
+  setupSmartWeaveEnv();
+  const caller = "<justin>";
+  const output = claim(
+    {
+      name: "rebar",
+      ticker: "rebar",
+      balances: {},
+      settings: [
+        ["communityLogo", "_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo"],
+        ["isTradeable", true],
+      ],
+      claims: [],
+      claimable: [
+        {
+          txID: "<test-claim>",
+          to: caller,
+          qty: 11,
+          from: "<tom>",
+        },
+      ],
+      divisibility: 6,
+    },
+    { caller, input: { txID: "<test-claim>", qty: 11 } }
+  );
+  const { state } = output;
+  assert.is(state.balances[caller], 11);
+  assert.is(state.claims[0], "<test-claim>");
+  assert.is(state.claimable.length, 0);
+});
+
+test.after(async () => {});
+
+test.run();
