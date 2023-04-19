3d6ad5a5382f45f7675257017e677cfd88d9aff9
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-19 16:12:46 -0500

    refactor: convert to pure functional

--- contract/src/write/mint.js
+++ contract/src/write/mint.js
@@ -1,19 +1,26 @@
 import { of, fromNullable } from "../hyper-either.js";
+import { setCallerBalance } from "../util.js";
+import { __, identity, assoc } from "ramda";
 
 export const mint = (state, action) => {
   return of({ state, action })
     .chain(fromNullable)
-    .fold(
-      () => {
-        throw new ContractError("An error occured.");
-      },
-      ({ state, action }) => {
-        const qty = Math.floor(parseInt(SmartWeave.transaction.reward) / 1e6);
-        // Set balance to 0 if it doesn't exist
-        if (!state.balances[action.caller]) state.balances[action.caller] = 0;
-
-        state.balances[action.caller] += qty;
-        return { state };
-      }
-    );
+    .map(setCallerBalance)
+    .map(updateBalance)
+    .map(assoc("state", __, {}))
+    .fold(() => {
+      throw new ContractError("An error occured.");
+    }, identity);
 };
+
+function updateBalance({ state, action }) {
+  return {
+    ...state,
+    balances: {
+      ...state.balances,
+      [action.caller]:
+        state.balances[action.caller] +
+        Math.floor(parseInt(SmartWeave.transaction.reward) / 1e6),
+    },
+  };
+}
