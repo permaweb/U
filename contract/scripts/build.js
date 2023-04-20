a1f3005de4b53fdd0ba175febcc9385aab51f9cc
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-20 11:05:00 -0500

    refactor: add 2 replacements to get BigNumber to build correctly; (was duplicating the BigNumber identifier)

--- contract/scripts/build.js
+++ contract/scripts/build.js
@@ -1,37 +1,49 @@
-import { build } from 'esbuild';
-import replace from 'replace-in-file';
+import { build } from "esbuild";
+import replace from "replace-in-file";
 
 build({
-  entryPoints: ['./src/contract.js'],
-  outdir: './dist',
+  entryPoints: ["./src/contract.js"],
+  outdir: "./dist",
   minify: false,
   bundle: true,
-  format: 'esm',
+  format: "esm",
 })
   .catch(() => process.exit(1))
 
   .finally(() => {
     replace.sync({
-      files: './dist/contract.js',
+      files: "./dist/contract.js",
       from: [/\(\(\) => {/g, /}\)\(\);/g],
-      to: '',
+      to: "",
       countMatches: true,
     });
     replace.sync({
-      files: './dist/contract.js',
-      from: ['async function handle'],
-      to: 'export async function handle',
+      files: "./dist/contract.js",
+      from: ["async function handle"],
+      to: "export async function handle",
       countMatches: true,
     });
     replace.sync({
-      files: './dist/contract.js',
+      files: "./dist/contract.js",
       from: [
         `
 export {
   handle
 };`,
       ],
-      to: '',
+      to: "",
+      countMatches: true,
+    });
+    replace.sync({
+      files: "./dist/contract.js",
+      from: ["var BigNumber = clone();"],
+      to: "var BigNumberClone = clone();",
+      countMatches: true,
+    });
+    replace.sync({
+      files: "./dist/contract.js",
+      from: ["var bignumber_default = BigNumber;"],
+      to: "var bignumber_default = BigNumberClone;",
       countMatches: true,
     });
   });
