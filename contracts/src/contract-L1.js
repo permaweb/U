13c446c467d4e050ca6c7948ad9bd9c009e84640
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-28 18:05:29 -0500

    refactor: filter requests coming in (remove 0, remove expired)

--- contracts/src/contract-L1.js
+++ contracts/src/contract-L1.js
@@ -1,8 +1,10 @@
 import { createMint } from "./write/create-mint.js";
 import { env } from "./smartweave.js";
+import { filterInvalid } from "./util.js";
 
 export async function handle(state, action) {
-  switch (input.function) {
+  const requests = filterInvalid(state.requests, env.block.height);
+  switch (action.input.function) {
     case "create-mint":
       return createMint(env)({ ...state, requests }, action);
     default:
