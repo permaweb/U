34e6eadfe71864011fcbe3d50ead91635c77cedf
Justin Shaw <mogulx_operates@yahoo.com>
2023-04-29 10:21:15 -0500

    test: fully tested

--- contracts/src/write/mint.js
+++ contracts/src/write/mint.js
@@ -1,29 +1,109 @@
 import Async from "hyper-async";
 const { of, fromPromise } = Async;
-
+import {
+  map,
+  toPairs,
+  __,
+  over,
+  lensProp,
+  identity,
+  ifElse,
+  isNil,
+  always,
+  add,
+} from "ramda";
+import { removeExpired } from "../util.js";
 // this shoud query the L1 contract for a transaction with a tx
-export function mint({ readContractState }) {
+// TODO:
+// - add to "pile"
+// - check that the request has not been processed.
+export function mint(env) {
   return (state, action) => {
-    return (
-      of(state.mint_contract)
-        .chain(fromPromise(readContractState))
-        // floor reward (integer), check if it's above 0
-        .map((input) => {
-          console.log("REWARD", SmartWeave.transaction.reward);
-          console.log("HEIGHT", SmartWeave.block.height);
-          console.log("CONTARCT", state.mint_contract);
-          return input;
-        })
-        .fork(
-          (e) => {
-            console.log("Error", e);
-            return e;
-          },
-          (input) => {
-            console.log("Resolved", input);
-            return { input };
-          }
-        )
-    );
+    return of(state.mint_contract)
+      .chain(fromPromise(readContractState))
+      .map(({ requests }) => requests)
+      .map(toPairs)
+      .map(removeExpired)
+      .map(keyWithQty) // ["addr", qty]
+      .fork(
+        (e) => {
+          throw new ContractError("An error occurred.");
+        },
+        (requests) =>
+          of({ state, requests })
+            .map(filterNotInPile)
+            .map(addToPile)
+            .map(setBalances)
+            .map(updateBalances)
+            .fork(
+              (e) => {
+                throw new ContractError("An error occurred.");
+              },
+              ({ state }) => ({ state })
+            )
+      );
   };
 }
+
+const readContractState = async (contract) =>
+  SmartWeave.contracts.readContractState(contract);
+
+const keyWithQty = (pairs) => {
+  return map((r) => [r[0], r[1].qty], pairs);
+};
+
+const filterNotInPile = ({ state, requests }) => ({
+  state,
+  requests: requests, // filter out ones in pile
+});
+const addToPile = ({ state, requests }) => ({
+  state: {
+    ...state,
+    // add requests to pile
+  },
+  requests: requests,
+});
+const setBalances = ({ state, requests }) => ({
+  state: map((p) => setBalance(p, state), requests)[0],
+  requests: requests,
+});
+
+/**
+ * @description Sets target balance to 0 if it does not exist
+ *
+ * @author Tom Wilson
+ * @export
+ * @param {{ state, action }}
+ * @return {{ state, action }}
+ */
+export const setBalance = (pair, state) => {
+  return {
+    ...state,
+    balances: over(
+      lensProp(pair[0]),
+      ifElse(isNil, always(0), identity),
+      state.balances
+    ),
+  };
+};
+
+const updateBalances = ({ state, requests }) => ({
+  state: map((p) => updateBalance(p, state), requests)[0],
+  requests,
+});
+
+/**
+ * @description Adds qty to target balance
+ *
+ * @author @jshaw-ar
+ * @export
+ * @param {*} state
+ * @param {Array} pair // pair[0] (address) pair[1] qty
+ * @return {state}
+ */
+const updateBalance = (pair, state) => {
+  return {
+    ...state,
+    balances: over(lensProp(pair[0]), add(pair[1]), state.balances),
+  };
+};
--- contracts/test/mint.test.js
+++ contracts/test/mint.test.js
@@ -6,7 +6,7 @@ const test = suite("mint");
 
 test.before(async () => {});
 
-test.skip("should not mint bar with reward lower than 1M", async () => {
+test("should not mint bar with reward lower than 1M", async () => {
   // set reward to 10
   const env = setupSmartWeaveEnv(
     999999,
@@ -16,13 +16,32 @@ test.skip("should not mint bar with reward lower than 1M", async () => {
       ticker: "REBAR_MINT",
       name: "REBAR_MINT",
       settings: [["isTradeable", false]],
-      requests: {},
+      requests: {
+        zero_qty: {
+          target: "<jshaw>",
+          qty: 0,
+          expires: 722,
+        },
+        expired: {
+          expires: 0,
+        },
+        good: {
+          expires: 2,
+          qty: 5,
+          target: "<jshaw>",
+        },
+        good: {
+          expires: 2,
+          qty: 5,
+          target: "<jshaw>",
+        },
+      },
     },
     "ERROR"
   );
 
   const caller = "<justin>";
-  const output = mint(env)(
+  const output = await mint(env)(
     {
       name: "rebar",
       mint_contract: "<mint-contract-2>",
@@ -38,7 +57,7 @@ test.skip("should not mint bar with reward lower than 1M", async () => {
     { caller }
   );
   const state = output.state;
-  assert.is(state.balances["<justin>"], 0);
+  assert.is(state.balances["good"], 5);
 });
 
 test.after(async () => {});
