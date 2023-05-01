a79f12d4df7198bfe715b828d6adb91c83c58eb0
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-01 08:12:15 -0500

    chore: cleanup

--- contracts/src/write/transfer.js
+++ contracts/src/write/transfer.js
@@ -10,40 +10,36 @@ import {
 } from "../util.js";
 
 export function transfer(state, action) {
-  return (
-    of({ state, action })
-      .chain(fromNullable)
-      .chain(ce(!action.input?.target, "Please specify a target."))
-      .chain(
-        ce(action.input?.target === action.caller, "Target cannot be caller.")
+  return of({ state, action })
+    .chain(fromNullable)
+    .chain(ce(!action.input?.target, "Please specify a target."))
+    .chain(
+      ce(action.input?.target === action.caller, "Target cannot be caller.")
+    )
+    .chain(
+      ce(
+        !isInteger(state.balances[action.caller]),
+        "Caller does not have a balance."
       )
-      .chain(
-        ce(
-          !isInteger(state.balances[action.caller]),
-          "Caller does not have a balance."
-        )
+    )
+    .chain(ce(!isInteger(action.input?.qty), "qty must be an integer."))
+    .chain(
+      ce(
+        action.input?.qty < 1,
+        "Invalid token transfer. qty must be an integer greater than 0."
       )
-      .chain(ce(!isInteger(action.input?.qty), "qty must be an integer."))
-      .chain(
-        ce(
-          action.input?.qty < 1,
-          "Invalid token transfer. qty must be an integer greater than 0."
-        )
+    )
+    .chain(
+      ce(
+        state.balances[action.caller] < action.input?.qty,
+        "Not enough tokens for transfer."
       )
-      .chain(
-        ce(
-          state.balances[action.caller] < action.input?.qty,
-          "Not enough tokens for transfer."
-        )
-      )
-      .map(setTargetBalance)
-      .map(subtractCallerBalance)
-      .map(addTargetBalance)
-      .map(pick(["state"]))
-      // .map(({ state }) => state)
-      // .map(assoc("state", __, {}))
-      .fold((msg) => {
-        throw new ContractError(msg || "An error occurred.");
-      }, identity)
-  );
+    )
+    .map(setTargetBalance)
+    .map(subtractCallerBalance)
+    .map(addTargetBalance)
+    .map(pick(["state"]))
+    .fold((msg) => {
+      throw new ContractError(msg || "An error occurred.");
+    }, identity);
 }
