df198d8ed475c850e8c3146ba8f91c98d0ae0eda
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-02 10:52:56 -0500

    chore: setup local deploy script (arlocal needs to be running)

---
+++ contracts/scripts/deploy-contract-local.mjs
@@ -0,0 +1,83 @@
+import { WarpFactory } from "warp-contracts";
+import { DeployPlugin } from "warp-contracts-plugin-deploy";
+// import ArLocal from "arlocal";
+import BigNumber from "bignumber.js";
+import { compose, prop, fromPairs, toPairs, map } from "ramda";
+import fs from "fs";
+import { execSync } from "child_process";
+
+const BAR = "VFr3Bk-uM-motpNNkkFg4lNW1BMmSfzqsVO551Ho4hA";
+const DRE = "https://cache-2.permaweb.tools";
+
+async function deploy(folder) {
+  try {
+    const BAR_STATE = await fetch(`${DRE}/contract/?id=${BAR}`)
+      .then((r) => r.json())
+      .then(prop("state"));
+    const balances = getBalances(BAR_STATE);
+
+    const warp = WarpFactory.forLocal().use(new DeployPlugin());
+    const wallet1 = await warp.generateWallet();
+    const contractSrcL1 = fs.readFileSync(`${folder}/contract-L1.js`, "utf8");
+    const stateFromFileL1 = JSON.parse(
+      fs.readFileSync(`${folder}/initial-state-L1.json`, "utf8")
+    );
+    const contractSrcSEQ = fs.readFileSync(`${folder}/contract-SEQ.js`, "utf8");
+    const stateFromFileSEQ = JSON.parse(
+      fs.readFileSync(`${folder}/initial-state-SEQ.json`, "utf8")
+    );
+
+    const initialStateL1 = {
+      ...stateFromFileL1,
+      ...{
+        owner: process.env.WALLET_ADDRESS,
+      },
+    };
+
+    const initialStateSEQ = {
+      ...stateFromFileSEQ,
+      ...{
+        owner: process.env.WALLET_ADDRESS,
+        balances,
+      },
+    };
+
+    const deployL1 = await warp.deploy({
+      wallet: wallet1.jwk,
+      initState: JSON.stringify(initialStateL1),
+      src: contractSrcL1,
+    });
+
+    const deploySEQ = await warp.deploy({
+      wallet: wallet1.jwk,
+      initState: JSON.stringify({
+        ...initialStateSEQ,
+        mint_contract: deployL1.contractTxId,
+      }),
+      src: contractSrcSEQ,
+    });
+    console.log(`L1 contractTxId ${deployL1.contractTxId}`);
+    console.log(`SEQ contractTxId ${deploySEQ.contractTxId}`);
+    execSync(
+      `(cd ../app && npm i && VITE_CONTRACT_L1=${deployL1.contractTxId} VITE_CONTRACT_SEQ=${deploySEQ.contractTxId} VITE_LOCAL=true npm run dev)`,
+      {
+        encoding: "utf8",
+        stdio: "inherit",
+      }
+    );
+  } catch (e) {
+    console.error(
+      "Could not deploy contracts. Make sure arlocal is running (npx arlocal)"
+    );
+  }
+}
+deploy(process.argv[2]).catch(console.log);
+
+function getBalances(state) {
+  return compose(
+    fromPairs,
+    map(([k, v]) => [k, new BigNumber(v).integerValue()]),
+    toPairs,
+    prop("balances")
+  )(state);
+}
