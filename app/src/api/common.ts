9ef1f445610e368f30fd40dcefb479765fccbca8
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-02 10:51:27 -0500

    feat: getWarpFactory func

---
+++ app/src/api/common.ts
@@ -0,0 +1,7 @@
+import { WarpFactory } from "warp-contracts";
+
+export function getWarpFactory() {
+  return import.meta.env.VITE_LOCAL === "true"
+    ? WarpFactory.forLocal()
+    : WarpFactory.forMainnet();
+}
