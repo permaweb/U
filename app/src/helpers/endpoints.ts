c6eff546b59280d17de066e5d88a124a70d7742f
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-03 17:48:30 -0500

    style: formatting

--- app/src/helpers/endpoints.ts
+++ app/src/helpers/endpoints.ts
@@ -1,3 +1,5 @@
 export function getBalanceEndpoint(wallet: string) {
-  return `${import.meta.env.VITE_LOCAL ? 'http://localhost:1984' : 'https://arweave.net'}/wallet/${wallet}/balance`;
+  return `${
+    import.meta.env.VITE_LOCAL ? 'http://localhost:1984' : 'https://arweave.net'
+  }/wallet/${wallet}/balance`;
 }
