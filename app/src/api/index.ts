31b887cf8d0049f42c36afbeb540481ecb7ca8e1
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-03 15:54:32 -0500

    refactor: export interfaces and getQueue out of contract api

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -1,11 +1,14 @@
 import { getState } from './get-state';
+import { getQueue } from './get-queue';
 import { createMint } from './create-mint';
 import { mint } from './mint';
 import { transfer } from './transfer';
+export * from './interface';
 
 export const env = {
   getState,
   createMint,
   mint,
   transfer,
+  getQueue,
 };
---
+++ app/src/api/interface/index.ts
@@ -0,0 +1,51 @@
+/**
+ * @author @jshaw-ar
+ * @export
+ * @interface StateSEQ
+ */
+export interface StateSEQ {
+  ticker: string;
+  name: string;
+  balances: {
+    [address: string]: number;
+  };
+  settings: any[];
+  claimable: Claimable[];
+  divisibility: 6;
+  pile: string[];
+}
+
+/**
+ * @author @jshaw-ar
+ * @interface Claimable
+ */
+interface Claimable {
+  to: string;
+  from: string;
+  txID: string;
+  qty: number;
+}
+
+/**
+ * @author @jshaw-ar
+ * @export
+ * @interface StateL1
+ */
+export interface StateL1 {
+  ticker: string;
+  name: string;
+  settings: any[];
+  requests: {
+    [tx: string]: MintRequest;
+  };
+}
+
+/**
+ * @author @jshaw-ar
+ * @interface MintRequest
+ */
+interface MintRequest {
+  target: string;
+  qty: number;
+  expires: number;
+}
