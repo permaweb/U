5ae53a5a3ed75f223bc4d5fbe92d50202e77557a
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-03 07:29:24 -0500

    fix: use rounded down .toNumber()

--- contracts/scripts/deploy-contract-prod.mjs
+++ contracts/scripts/deploy-contract-prod.mjs
@@ -1,34 +1,34 @@
-import { WarpFactory } from "warp-contracts";
-import { DeployPlugin, ArweaveSigner } from "warp-contracts-plugin-deploy";
-import BigNumber from "bignumber.js";
-import { compose, prop, fromPairs, toPairs, map } from "ramda";
+import { WarpFactory } from 'warp-contracts';
+import { DeployPlugin, ArweaveSigner } from 'warp-contracts-plugin-deploy';
+import BigNumber from 'bignumber.js';
+import { compose, prop, fromPairs, toPairs, map } from 'ramda';
 
-import fs from "fs";
+import fs from 'fs';
 
-const BAR = "VFr3Bk-uM-motpNNkkFg4lNW1BMmSfzqsVO551Ho4hA";
-const DRE = "https://cache-2.permaweb.tools";
+const BAR = 'VFr3Bk-uM-motpNNkkFg4lNW1BMmSfzqsVO551Ho4hA';
+const DRE = 'https://cache-2.permaweb.tools';
 
 async function deploy(folder) {
   const BAR_STATE = await fetch(`${DRE}/contract/?id=${BAR}`)
     .then((r) => r.json())
-    .then(prop("state"));
+    .then(prop('state'));
   const balances = getBalances(BAR_STATE);
 
   const jwk = JSON.parse(
     fs.readFileSync(process.env.PATH_TO_WALLET).toString()
   );
   const warp = WarpFactory.forMainnet().use(new DeployPlugin());
-  const contractSrcL1 = fs.readFileSync(`${folder}/contract-L1.js`, "utf8");
+  const contractSrcL1 = fs.readFileSync(`${folder}/contract-L1.js`, 'utf8');
   const stateFromFileL1 = JSON.parse(
-    fs.readFileSync(`${folder}/initial-state-L1.json`, "utf8")
+    fs.readFileSync(`${folder}/initial-state-L1.json`, 'utf8')
   );
-  const contractSrcSEQ = fs.readFileSync(`${folder}/contract-SEQ.js`, "utf8");
+  const contractSrcSEQ = fs.readFileSync(`${folder}/contract-SEQ.js`, 'utf8');
   const stateFromFileSEQ = JSON.parse(
-    fs.readFileSync(`${folder}/initial-state-SEQ.json`, "utf8")
+    fs.readFileSync(`${folder}/initial-state-SEQ.json`, 'utf8')
   );
   if (!process.env.WALLET_ADDRESS) {
     console.error(
-      "Set proces.env.WALLET_ADDRESS to your wallet addres. eg. 9x24zjvs9DA5zAz2DmqBWAg6XcxrrE-8w3EkpwRm4e4"
+      'Set proces.env.WALLET_ADDRESS to your wallet addres. eg. 9x24zjvs9DA5zAz2DmqBWAg6XcxrrE-8w3EkpwRm4e4'
     );
     process.exit(1);
   }
@@ -69,8 +69,11 @@ deploy(process.argv[2]).catch(console.log);
 function getBalances(state) {
   return compose(
     fromPairs,
-    map(([k, v]) => [k, new BigNumber(v).integerValue()]),
+    map(([k, v]) => [
+      k,
+      new BigNumber(v).integerValue(BigNumber.ROUND_DOWN).toNumber(),
+    ]),
     toPairs,
-    prop("balances")
+    prop('balances')
   )(state);
 }
