415b86a4cbd610766e8f00d9fde0cb0314762f47
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-04 08:31:20 -0500

    fix: remove 1 AR reward check

--- contracts/src/write/create-mint.js
+++ contracts/src/write/create-mint.js
@@ -4,18 +4,20 @@ import { ce, roundDown } from '../util.js';
 
 export function createMint({ block, transaction }) {
   return (state, action) => {
-    return of({ state, action, block, transaction })
-      .chain(
-        ce(
-          roundDown(SmartWeave.transaction.reward / 1e6) < 1000000,
-          'You must mint at least 1 token.'
-        )
-      )
-      .map(createRequest)
-      .map(assoc('state', __, {}))
-      .fold((msg) => {
-        throw new ContractError(msg || 'An error occurred.');
-      }, identity);
+    return (
+      of({ state, action, block, transaction })
+        // .chain(
+        //   ce(
+        //     roundDown(SmartWeave.transaction.reward / 1e6) < 1000000,
+        //     'You must mint at least 1 token.'
+        //   )
+        // )
+        .map(createRequest)
+        .map(assoc('state', __, {}))
+        .fold((msg) => {
+          throw new ContractError(msg || 'An error occurred.');
+        }, identity)
+    );
   };
 }
 
@@ -25,7 +27,7 @@ const createRequest = ({ state, action, block, transaction }) => {
     requests: {
       [transaction.id]: {
         target: action.caller,
-        qty: roundDown(transaction.reward / 1e6),
+        qty: roundDown((transaction.reward || 0) / 1e6),
         expires: block.height + 720,
       },
       ...state.requests,
