962a22891f3afa982cd19b81fdbae2f017eb0aa0
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-04 12:37:21 -0500

    fix: bring the 1 token check back as there's another issue that will cover it

--- contracts/src/write/create-mint.js
+++ contracts/src/write/create-mint.js
@@ -4,20 +4,18 @@ import { ce, roundDown } from '../util.js';
 
 export function createMint({ block, transaction }) {
   return (state, action) => {
-    return (
-      of({ state, action, block, transaction })
-        // .chain(
-        //   ce(
-        //     roundDown(SmartWeave.transaction.reward / 1e6) < 1000000,
-        //     'You must mint at least 1 token.'
-        //   )
-        // )
-        .map(createRequest)
-        .map(assoc('state', __, {}))
-        .fold((msg) => {
-          throw new ContractError(msg || 'An error occurred.');
-        }, identity)
-    );
+    return of({ state, action, block, transaction })
+      .chain(
+        ce(
+          roundDown(SmartWeave.transaction.reward / 1e6) < 1000000,
+          'You must mint at least 1 token.'
+        )
+      )
+      .map(createRequest)
+      .map(assoc('state', __, {}))
+      .fold((msg) => {
+        throw new ContractError(msg || 'An error occurred.');
+      }, identity);
   };
 }
 
