22a586b834b979c8a5eb5c7f5730b5ed39511cc6
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-05 08:46:02 -0500

    feat: killswitch / whitelist (for testing)

--- contracts/src/contract-L1.js
+++ contracts/src/contract-L1.js
@@ -1,8 +1,15 @@
-import { createMint } from './write/create-mint.js';
 import { getQueue } from './read/get-queue.js';
-import { filterInvalid } from './util.js';
+import { createMint } from './write/create-mint.js';
+import { kill } from './write/kill.js';
+import { add, remove } from './write/whitelist.js';
+import { filterInvalid, checkKillSwitch, checkWhitelist } from './util.js';
 
 export async function handle(state, action) {
+  // GATE
+  checkKillSwitch(state.killswitch);
+  checkWhitelist(state.whitelist, action.caller);
+  // GATE END
+
   const env = {
     readContractState: SmartWeave.contracts.readContractState.bind(SmartWeave),
     viewContractState: SmartWeave.contracts.viewContractState.bind(SmartWeave),
@@ -14,10 +21,17 @@ export async function handle(state, action) {
   const requests = filterInvalid(state.requests, env.block.height);
 
   switch (action.input.function) {
+    case 'kill':
+      return await kill(state, action);
+    case 'add':
+      return await add(state, action);
+    case 'remove':
+      return await remove(state, action);
     case 'create-mint':
       return createMint(env)({ ...state, requests }, action);
     case 'get-queue':
       return getQueue(state, action);
+
     default:
       throw new ContractError(
         `No function supplied or function not recognized`
