e847cdc8094db83994ec7323b56446ed20ea1d28
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-07 12:54:55 -0500

    test: only mint new requests

--- contracts/test/mint.test.js
+++ contracts/test/mint.test.js
@@ -7,30 +7,67 @@ const test = suite('mint');
 
 test.before(async () => {});
 
-test('should not mint bar with reward lower than 1M', async () => {
+test('should only mint new requests', async () => {
   // set reward to 10
   const env = setupSmartWeaveEnv(999999, 1, '<tx>', undefined, 'ERROR', {
-    zero_qty: {
+    not_expired: {
       target: '<jshaw>',
-      qty: 0,
-      expires: 722,
+      qty: 25,
+      expires: 721,
     },
-    expired: {
+    expired2: {
       expires: 0,
-    },
-    good: {
-      expires: 2,
       qty: 5,
       target: '<jshaw>',
     },
+    expired3: {
+      expires: 0,
+      qty: 5,
+      target: '<jshaw>',
+    },
+    expired4: {
+      expires: 0,
+      qty: 5,
+      target: '<jshaw>',
+    },
+    processed: {
+      expires: 721,
+      qty: 5,
+      target: '<jshaw>',
+    },
+  });
+
+  const caller = '<justin>';
+
+  const output = await mint(env)(
+    {
+      ticker: 'REBAR',
+      name: 'REBAR',
+      balances: {},
+      settings: [['isTradeable', true]],
+      claimable: [],
+      divisibility: 6,
+      mint_contract: '<mint-contract>',
+      pile: ['processed'],
+    },
+    { caller }
+  ).toPromise();
+  const state = output.state;
+  assert.is(state.balances['<jshaw>'], 25);
+  assert.is(state.pile.length, 2);
+});
+
+test('should work with no requests TODO: udpate this test name', async () => {
+  // set reward to 10
+  const env = setupSmartWeaveEnv(1000000, 0, '<tx>', undefined, 'ERROR', {
     good: {
       expires: 2,
-      qty: 5,
+      qty: 7,
       target: '<jshaw>',
     },
     good2: {
       expires: 2,
-      qty: 5,
+      qty: 7,
       target: '<jshaw>',
     },
     processed: {
@@ -56,31 +93,7 @@ test('should not mint bar with reward lower than 1M', async () => {
     { caller }
   ).toPromise();
   const state = output.state;
-  assert.is(state.balances['<jshaw>'], undefined);
-  assert.is(state.pile.filter((v) => v === 'processed').length, 1);
-});
-
-test('should work with no requests TODO: udpate this test name', async () => {
-  // set reward to 10
-  const env = setupSmartWeaveEnv(999999, 10, '<tx>', undefined, 'ERROR', {});
-
-  const caller = '<justin>';
-
-  const output = await mint(env)(
-    {
-      ticker: 'REBAR',
-      name: 'REBAR',
-      balances: {},
-      settings: [['isTradeable', true]],
-      claimable: [],
-      divisibility: 6,
-      mint_contract: '<mint-contract>',
-      pile: ['processed'],
-    },
-    { caller }
-  ).toPromise();
-  const state = output.state;
-  assert.is(toPairs(state.balances).length, 0);
+  assert.is(state.balances['<jshaw>'], 14);
   assert.is(state.pile.filter((v) => v === 'processed').length, 1);
 });
 
