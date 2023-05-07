5565340bff03384ea4c4b02e7a6a120d2ad88974
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-07 07:23:39 -0500

    style: formatting

--- app/src/views/Exchange/Swap/styles.ts
+++ app/src/views/Exchange/Swap/styles.ts
@@ -7,12 +7,12 @@ import { MintStatusType } from 'helpers/types';
 
 function getDetailLineBackground(type: MintStatusType, theme: DefaultTheme) {
   switch (type) {
-      case 'confirmed':
-          return theme.colors.orderLine.confirmed.background;
-      case 'pending':
-          return theme.colors.orderLine.pending.background;
-      default:
-          return theme.colors.orderLine.default.background;
+    case 'confirmed':
+      return theme.colors.orderLine.confirmed.background;
+    case 'pending':
+      return theme.colors.orderLine.pending.background;
+    default:
+      return theme.colors.orderLine.default.background;
   }
 }
 
@@ -72,70 +72,76 @@ export const AWrapper = styled.div`
 `;
 
 export const DetailWrapper = styled.div`
-    margin: 40px 0 0 0;
-    > * {
-        &:last-child {
-            border-bottom-left-radius: ${STYLING.dimensions.borderRadius};
-            border-bottom-right-radius: ${STYLING.dimensions.borderRadius};
-        }
+  margin: 40px 0 0 0;
+  > * {
+    &:last-child {
+      border-bottom-left-radius: ${STYLING.dimensions.borderRadius};
+      border-bottom-right-radius: ${STYLING.dimensions.borderRadius};
     }
+  }
 `;
 
 export const DHeader = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    padding: 15.5px 20px;
-    border-top: 1px solid ${(props) => props.theme.colors.border.alt3};
-    border-left: 1px solid ${(props) => props.theme.colors.border.alt3};
-    border-right: 1px solid ${(props) => props.theme.colors.border.alt3};
-    background: ${(props) => props.theme.colors.orderLine.header.background};
-    border-top-left-radius: ${STYLING.dimensions.borderRadius};
-    border-top-right-radius: ${STYLING.dimensions.borderRadius};
-    p {
-      font-size: ${(props) => props.theme.typography.size.small};
-      font-weight: ${(props) => props.theme.typography.weight.medium};
-      color: ${(props) => props.theme.colors.font.primary.alt1};
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  padding: 15.5px 20px;
+  border-top: 1px solid ${(props) => props.theme.colors.border.alt3};
+  border-left: 1px solid ${(props) => props.theme.colors.border.alt3};
+  border-right: 1px solid ${(props) => props.theme.colors.border.alt3};
+  background: ${(props) => props.theme.colors.orderLine.header.background};
+  border-top-left-radius: ${STYLING.dimensions.borderRadius};
+  border-top-right-radius: ${STYLING.dimensions.borderRadius};
+  p {
+    font-size: ${(props) => props.theme.typography.size.small};
+    font-weight: ${(props) => props.theme.typography.weight.medium};
+    color: ${(props) => props.theme.colors.font.primary.alt1};
   }
 `;
 
 export const DetailGroup = styled.div``;
 
-export const DetailLine = styled.div<{ type: MintStatusType, ownerLine: boolean }>`
-    display: flex;
-    justify-content: space-between;
-    padding: 6.5px 20px;
-    background: ${(props) => props.ownerLine ? props.theme.colors.orderLine.ownerLine.background : getDetailLineBackground(props.type, props.theme)};
-    p {
-        font-size: ${(props) => props.theme.typography.size.xSmall};
-        font-weight: ${(props) => props.theme.typography.weight.medium};
-		color: ${(props) => props.theme.colors.font.primary.base};
-    }
+export const DetailLine = styled.div<{
+  type: MintStatusType;
+  ownerLine: boolean;
+}>`
+  display: flex;
+  justify-content: space-between;
+  padding: 6.5px 20px;
+  background: ${(props) =>
+    props.ownerLine
+      ? props.theme.colors.orderLine.ownerLine.background
+      : getDetailLineBackground(props.type, props.theme)};
+  p {
+    font-size: ${(props) => props.theme.typography.size.xSmall};
+    font-weight: ${(props) => props.theme.typography.weight.medium};
+    color: ${(props) => props.theme.colors.font.primary.base};
+  }
 `;
 
 export const DetailSubheader = styled.div`
-    display: flex;
-    justify-content: space-between;
-    padding: 6.5px 20px;
-    border-top: 1px solid ${(props) => props.theme.colors.border.alt3};
-    border-bottom: 1px solid ${(props) => props.theme.colors.border.alt3};
-    background: ${(props) => props.theme.colors.orderLine.subheader.background};
-    p {
-        font-size: ${(props) => props.theme.typography.size.xSmall};
-        font-weight: ${(props) => props.theme.typography.weight.medium};
-        color: ${(props) => props.theme.colors.font.primary.alt1};
-    }
+  display: flex;
+  justify-content: space-between;
+  padding: 6.5px 20px;
+  border-top: 1px solid ${(props) => props.theme.colors.border.alt3};
+  border-bottom: 1px solid ${(props) => props.theme.colors.border.alt3};
+  background: ${(props) => props.theme.colors.orderLine.subheader.background};
+  p {
+    font-size: ${(props) => props.theme.typography.size.xSmall};
+    font-weight: ${(props) => props.theme.typography.weight.medium};
+    color: ${(props) => props.theme.colors.font.primary.alt1};
+  }
 `;
 
 export const DetailValue = styled.div<{ widthPercentage: number }>`
-    width: ${(props) => `${props.widthPercentage.toString()}`}%;
-    p {
-        font-size: ${(props) => props.theme.typography.size.xSmall};
-        font-weight: ${(props) => props.theme.typography.weight.medium};
-		color: ${(props) => props.theme.colors.font.primary.base};
-    }
+  width: ${(props) => `${props.widthPercentage.toString()}`}%;
+  p {
+    font-size: ${(props) => props.theme.typography.size.xSmall};
+    font-weight: ${(props) => props.theme.typography.weight.medium};
+    color: ${(props) => props.theme.colors.font.primary.base};
+  }
 `;
 
 export const Qty = styled.p`
-    color: ${(props) => props.theme.colors.font.primary.positive} !important;
+  color: ${(props) => props.theme.colors.font.primary.positive} !important;
 `;
