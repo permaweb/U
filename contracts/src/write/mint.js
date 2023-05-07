ca86b05adedbc805037457790be41e661592b1fb
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-07 11:58:20 -0500

    fix: remove 0 filter as it's impossible to `create-mint` for less than 1 feron

--- contracts/src/write/mint.js
+++ contracts/src/write/mint.js
@@ -38,7 +38,6 @@ export function mint({ viewContractState, block }) {
       .map(prop('result'))
       .map(toPairs)
       .map(removeExpired(__, block.height))
-      .map(removeZero)
       .map(notInPile(state, __))
       .map((requests) => ({ state, requests }))
       .map(addToPile)
