cff8adb32ecb6b9f34b082404c21dd80efbbce28
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-11 14:54:18 -0500

    refactor: mock KV

--- contracts/test/setup.js
+++ contracts/test/setup.js
@@ -1,11 +1,17 @@
+import { toPairs } from 'ramda';
+
 export function setupSmartWeaveEnv(
   reward,
   height,
   id,
   readContractState, // pass `Promise.reject("<READ-ERROR>")` for error
   write, // pass `Promise.reject("<WRITE-ERROR>")` for error
-  viewContractState
+  viewContractState,
+  balances // Pass balances like {"<addr>": "<qty>"} and a proxy map will be built to mock SmartWeave.KV
 ) {
+  const pairs = toPairs(balances);
+  const map = new Map(pairs);
+
   globalThis.SmartWeave = {
     transaction: {
       reward: reward || 1,
@@ -21,9 +27,15 @@ export function setupSmartWeaveEnv(
         result: viewContractState,
       }),
     },
+    kv: {
+      get: async (key) => map.get(key),
+      put: async (caller, qty) => map.set(caller, qty),
+      del: async (key) => map.delete(key),
+    },
   };
   globalThis.ContractError = ContractError;
   return {
+    kv: SmartWeave.kv,
     readContractState: async (contract) =>
       SmartWeave.contracts.readContractState(contract),
     viewContractState: async (contract, input) =>
