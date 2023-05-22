ab3866cfb3280fb6063f5de44350c9fb55e7f750
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-22 08:43:29 -0500

    refactor: simplify to new interface

--- app/src/api/transfer.ts
+++ app/src/api/transfer.ts
@@ -2,46 +2,18 @@ import Async from 'hyper-async';
 import { getWarpFactory, syncState } from './common';
 const { of, fromPromise } = Async;
 import BigNumber from 'bignumber.js';
-import { StateSEQ } from './interface';
-import {
-  over,
-  ifElse,
-  identity,
-  lensProp,
-  always,
-  isNil,
-  add,
-  subtract,
-  __,
-} from 'ramda';
 
 export interface TransferInput {
   contractId: string;
   from: string;
   target: string;
   qty: number;
-  state: StateSEQ;
 }
 
 export function transfer(input: TransferInput) {
   return of(input)
     .chain((input: TransferInput) => fromPromise(warpTransfer)(input))
-    .map(setTargetBalance)
-    .map(subtractCallerBalance)
-    .map(addTargetBalance)
-    .fork(
-      (e: any) => {
-        console.log(e);
-        return { error: 'There was an error fetching the contract state' };
-      },
-      (output: {
-        state: StateSEQ;
-        action: { caller: string; input: { qty: number; target: string } };
-      }) => {
-        console.log('Transfer State', output.state);
-        return { ...output.state };
-      }
-    );
+    .toPromise();
 }
 
 const warpTransfer = async (input: TransferInput) => {
@@ -54,97 +26,22 @@ const warpTransfer = async (input: TransferInput) => {
     .connect('use_wallet')
     .setEvaluationOptions({
       internalWrites: true,
+      unsafeClient: 'skip',
+      useKVStorage: true,
+      useConstructor: true,
       allowBigInt: true,
     });
   const newQty = new BigNumber(qty * 1e6)
     .integerValue(BigNumber.ROUND_DOWN)
     .toNumber();
-  contract.writeInteraction({
+
+  const interaction = await contract.writeInteraction({
     function: 'transfer',
     target,
     qty: newQty,
   });
 
-  return {
-    state: { ...input.state },
-    action: { caller: from, input: { target, qty: newQty } },
-  };
+  if (interaction?.originalTxId) {
+    return interaction;
+  }
 };
-
-/**
- * @description Sets target balance to 0 if it does not exist
- *
- * @author Tom Wilson
- * @export
- * @param {{ state, action }}
- * @return {{ state, action }}
- */
-export function setTargetBalance(input: {
-  state: StateSEQ;
-  action: { caller: string; input: { qty: number; target: string } };
-}) {
-  const { state, action } = input;
-  return {
-    state: {
-      ...state,
-      balances: over(
-        lensProp(action.input.target),
-        ifElse(isNil, always(0), identity),
-        state.balances
-      ),
-    },
-    action,
-  };
-}
-
-/**
- * @author @jshaw-ar
- * @export
- * @param {{
- *   state: StateSEQ;
- *   action: { caller: string; input: { qty: number; target: string } };
- * }} input
- * @return {*}
- */
-export function subtractCallerBalance(input: {
-  state: StateSEQ;
-  action: { caller: string; input: { qty: number; target: string } };
-}) {
-  const { state, action } = input;
-  return {
-    state: {
-      ...state,
-      balances: over(
-        lensProp(action.caller),
-        subtract(__, action.input.qty),
-        state.balances
-      ),
-    },
-    action,
-  };
-}
-
-/**
- * @description Adds qty from caller balance
- *
- * @author @jshaw-ar
- * @param {{ state, action }}
- * @return {{ state, action }}
- */
-export function addTargetBalance(input: {
-  state: StateSEQ;
-  action: { caller: string; input: { qty: number; target: string } };
-}) {
-  const { state, action } = input;
-  return {
-    state: {
-      ...state,
-      balances: over(
-        lensProp(action.input.target),
-        add(action.input.qty),
-        state.balances
-      ),
-    },
-    action,
-  };
-}
