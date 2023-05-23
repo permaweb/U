10c2903609dbadea89b823e82c95aaf2d279aac3
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-23 12:23:25 -0500

    fix: initialize with state.balances

--- contracts/src/write/initialize.js
+++ contracts/src/write/initialize.js
@@ -1,9 +1,7 @@
 export async function initialize(state, action) {
   if (state.initialized) return state;
-  await Promise.all(
-    Object.keys(action.input.initialBalances).map((k) =>
-      SmartWeave.kv.put(k, action.input.initialBalances[k])
-    )
+  Object.keys(action.input.initialBalances).map(
+    (k) => (state.balances[k] = action.input.initialBalances[k])
   );
   state.initialized = true;
   return { state };
