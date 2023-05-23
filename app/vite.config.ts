9f1f5cfb8cf2227d1fa6d5cf9c3f112044305c09
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-23 14:20:07 -0500

    chore: add base config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: [
