16620a997f1bdb0525e5adb6f43b175a2eacb2fd
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-25 08:51:45 -0500

    chore: add deploy action

--- app/scripts/deploy-action.mjs
+++ app/scripts/deploy-action.mjs
@@ -1,34 +1,13 @@
 import { readFileSync } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import Bundlr from '@bundlr-network/client';
 import { WarpFactory, defaultCacheOptions } from 'warp-contracts';
-import Arweave from 'arweave';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const ANT = 'tudrGap6uMQ80zdFZsjAUkSp4Ea8YYsaKgskewqNWSU'; // rebar.arweave.dev
-const arweave = Arweave.init({
-  host: 'arweave.net',
-  port: 443,
-  protocol: 'https',
-});
-
-const filePath = path.join(__dirname, 'contracts.json');
-const contracts = JSON.parse(readFileSync(filePath, 'utf8'));
-
-console.log('Contracts', contracts);
 // const jwk = JSON.parse(fs.readFileSync('../wallet.json', 'utf-8'))
 const jwk = JSON.parse(
   Buffer.from(process.env.WALLET, 'base64').toString('utf-8')
 );
 
-const bundlr = new Bundlr.default(
-  'https://node2.bundlr.network',
-  'arweave',
-  jwk
-);
+const bundlr = new Bundlr('https://node2.bundlr.network', 'arweave', jwk);
 const warp = WarpFactory.custom(arweave, defaultCacheOptions, 'mainnet')
   .useArweaveGateway()
   .build();
