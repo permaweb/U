48a4034fe8c35012e2835c21a8a2a0416dc799ce
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-29 15:28:00 -0500

    fix: remove env

--- contracts/src/write/allow.js
+++ contracts/src/write/allow.js
@@ -10,43 +10,41 @@ import { ce, isInteger, roundDown } from '../util.js';
  * @param {*} action
  * @return {*} {state}
  */
-export function allow({ transaction }) {
-  return (state, action) => {
-    return of(action.caller)
-      .chain(fromNullable)
-      .chain(ce(!action.input?.target, 'Please specify a target.'))
-      .chain(
-        ce(action.input?.target === action.caller, 'Target cannot be caller.')
-      )
+export function allow(state, action) {
+  return of(action.caller)
+    .chain(fromNullable)
+    .chain(ce(!action.input?.target, 'Please specify a target.'))
+    .chain(
+      ce(action.input?.target === action.caller, 'Target cannot be caller.')
+    )
 
-      .chain(ce(!isInteger(action.input?.qty), 'qty must be an integer.'))
-      .chain(
-        ce(
-          roundDown(action.input?.qty) < 1,
-          'Invalid token transfer. qty must be an integer greater than 0.'
-        )
+    .chain(ce(!isInteger(action.input?.qty), 'qty must be an integer.'))
+    .chain(
+      ce(
+        roundDown(action.input?.qty) < 1,
+        'Invalid token transfer. qty must be an integer greater than 0.'
       )
-      .chain(
-        ce(
-          (state.balances[action.caller] || 0) < roundDown(action.input?.qty),
-          'Not enough tokens for transfer.'
-        )
+    )
+    .chain(
+      ce(
+        (state.balances[action.caller] || 0) < roundDown(action.input?.qty),
+        'Not enough tokens for transfer.'
       )
-      .map((caller) => {
-        const safeQty = roundDown(action.input.qty);
-        state.balances[caller] -= safeQty;
-        state.claimable.push({
-          from: caller,
-          to: action.input.target,
-          qty: safeQty,
-          txID: transaction.id,
-        });
-      })
-      .fold(
-        (msg) => {
-          throw new ContractError(msg || 'An error occurred.');
-        },
-        () => ({ state })
-      );
-  };
+    )
+    .map((caller) => {
+      const safeQty = roundDown(action.input.qty);
+      state.balances[caller] -= safeQty;
+      state.claimable.push({
+        from: caller,
+        to: action.input.target,
+        qty: safeQty,
+        txID: SmartWeave.transaction.id,
+      });
+    })
+    .fold(
+      (msg) => {
+        throw new ContractError(msg || 'An error occurred.');
+      },
+      () => ({ state })
+    );
 }
