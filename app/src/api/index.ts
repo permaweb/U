34c9d7b958feebe557b1a61bb03385ef9229c20f
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-29 16:16:03 -0500

    feat: getPollingTx

--- app/src/api/index.ts
+++ app/src/api/index.ts
@@ -1,18 +1,17 @@
 import { getState } from './get-state';
 import { burn } from './burn';
-import { mint } from './mint';
 import { transfer } from './transfer';
 import { claim } from './claim';
-import { pollMint } from './poll-mint';
+import { getPollingTx, pollMint } from './poll-mint';
 import { getRebarBalance } from './get-rebar-balance';
 export * from './interface';
 
 export const env = {
   getState,
   burn,
-  mint,
   transfer,
   getRebarBalance,
   claim,
   pollMint,
+  getPollingTx,
 };
