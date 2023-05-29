d6f1043d0135cb990ff2d319279a24d39b1778cf
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-29 14:41:35 -0500

    fix: build with only l2

--- app/scripts/build.mjs
+++ app/scripts/build.mjs
@@ -9,10 +9,7 @@ const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, '../contracts.json');
 const contracts = JSON.parse(readFileSync(filePath, 'utf8'));
 
-execSync(
-  `(npm i && tsc && VITE_CONTRACT_L1=${contracts.l1} VITE_CONTRACT_SEQ=${contracts.l2} vite build)`,
-  {
-    encoding: 'utf8',
-    stdio: 'inherit',
-  }
-);
+execSync(`(npm i && tsc && VITE_CONTRACT=${contracts.l2} vite build)`, {
+  encoding: 'utf8',
+  stdio: 'inherit',
+});
