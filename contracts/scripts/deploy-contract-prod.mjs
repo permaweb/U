e80ef74ab2f482d1f941dd40566aa770f3db83bf
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-30 11:20:02 -0500

    fix: omit the address that wasn't an integer

--- contracts/scripts/deploy-contract-prod.mjs
+++ contracts/scripts/deploy-contract-prod.mjs
@@ -5,9 +5,7 @@ import BigNumber from 'bignumber.js';
 
 import fs from 'fs';
 export function roundDown(v) {
-  const intValue = new BigNumber(v).integerValue(BigNumber.ROUND_DOWN);
-  console.log('VALUE', intValue);
-  return intValue.toNumber();
+  return new BigNumber(v).integerValue(BigNumber.ROUND_DOWN).toNumber();
 }
 async function deploy(folder) {
   const balances = await fetch(
@@ -15,6 +13,7 @@ async function deploy(folder) {
   )
     .then((res) => res.json())
     .then((result) => result.state.balances)
+    .then(map((v) => roundDown(v)))
     .then(
       omit([
         'vh-NTHVvlKZqRxc8LyyTNok65yQ55a_PJ1zWLb9G2JI',
@@ -22,63 +21,57 @@ async function deploy(folder) {
         'vLRHFqCw1uHu75xqB4fCDW-QxpkpJxBtFD9g4QYUbfw',
         '89tR0-C1m3_sCWCoVCChg4gFYKdiH5_ZDyZpdJ2DDRw',
         'uf_FqRvLqjnFMc8ZzGkF4qWKuNmUIQcYP0tPlCGORQk',
+        'VYDyg7TPf5tB4wqeCOklGx5VWjrbRFkDnsCuFaWhtb8',
       ])
-    )
-    .then(map((v) => roundDown(v)));
-
-  console.log(
-    'BALANCE',
-    balances['VYDyg7TPf5tB4wqeCOklGx5VWjrbRFkDnsCuFaWhtb8']
+    );
+  const jwk = JSON.parse(
+    fs.readFileSync(process.env.PATH_TO_WALLET).toString()
   );
+  const warp = WarpFactory.forMainnet(defaultCacheOptions, true).use(
+    new DeployPlugin()
+  );
+  const contractSrc = fs.readFileSync(`${folder}/contract.js`, 'utf8');
+  const stateFromFile = JSON.parse(
+    fs.readFileSync(`${folder}/initial-state.json`, 'utf8')
+  );
+  if (!process.env.WALLET_ADDRESS) {
+    console.error(
+      'Set proces.env.WALLET_ADDRESS to your wallet addres. eg. 9x24zjvs9DA5zAz2DmqBWAg6XcxrrE-8w3EkpwRm4e4'
+    );
+    process.exit(1);
+  }
 
-  // const jwk = JSON.parse(
-  //   fs.readFileSync(process.env.PATH_TO_WALLET).toString()
-  // );
-  // const warp = WarpFactory.forMainnet(defaultCacheOptions, true).use(
-  //   new DeployPlugin()
-  // );
-  // const contractSrc = fs.readFileSync(`${folder}/contract.js`, 'utf8');
-  // const stateFromFile = JSON.parse(
-  //   fs.readFileSync(`${folder}/initial-state.json`, 'utf8')
-  // );
-  // if (!process.env.WALLET_ADDRESS) {
-  //   console.error(
-  //     'Set proces.env.WALLET_ADDRESS to your wallet addres. eg. 9x24zjvs9DA5zAz2DmqBWAg6XcxrrE-8w3EkpwRm4e4'
-  //   );
-  //   process.exit(1);
-  // }
+  const initialState = {
+    ...stateFromFile,
+    ...{
+      owner: process.env.WALLET_ADDRESS,
+    },
+  };
 
-  // const initialState = {
-  //   ...stateFromFile,
-  //   ...{
-  //     owner: process.env.WALLET_ADDRESS,
-  //   },
-  // };
-
-  // const deployed = await warp.deploy(
-  //   {
-  //     // wallet: new ArweaveSigner(jwk),
-  //     wallet: jwk,
-  //     initState: JSON.stringify({
-  //       ...initialState,
-  //       balances,
-  //     }),
-  //     src: contractSrc,
-  //     evaluationManifest: {
-  //       evaluationOptions: {
-  //         sourceType: SourceType.BOTH,
-  //         internalWrites: true,
-  //         allowBigInt: true,
-  //         unsafeClient: 'skip',
-  //       },
-  //     },
-  //   },
-  //   {
-  //     disabledBundling: true,
-  //   }
-  // );
-  // console.log(`SEQ contractTxId ${deployed.contractTxId}`);
-  // await waitForConfirmation(deployed.contractTxId);
+  const deployed = await warp.deploy(
+    {
+      // wallet: new ArweaveSigner(jwk),
+      wallet: jwk,
+      initState: JSON.stringify({
+        ...initialState,
+        balances,
+      }),
+      src: contractSrc,
+      evaluationManifest: {
+        evaluationOptions: {
+          sourceType: SourceType.BOTH,
+          internalWrites: true,
+          allowBigInt: true,
+          unsafeClient: 'skip',
+        },
+      },
+    },
+    {
+      disabledBundling: true,
+    }
+  );
+  console.log(`SEQ contractTxId ${deployed.contractTxId}`);
+  await waitForConfirmation(deployed.contractTxId);
 }
 deploy(process.argv[2]).catch(console.log);
 
