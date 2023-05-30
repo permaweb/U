d42e84ba3754880e68045b7f0fa72be4ce106e37
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-30 09:43:39 -0500

    chore(wip): testing balance migration

--- contracts/scripts/deploy-contract-prod.mjs
+++ contracts/scripts/deploy-contract-prod.mjs
@@ -5,7 +5,9 @@ import BigNumber from 'bignumber.js';
 
 import fs from 'fs';
 export function roundDown(v) {
-  return new BigNumber(v).integerValue(BigNumber.ROUND_DOWN).toNumber();
+  const intValue = new BigNumber(v).integerValue(BigNumber.ROUND_DOWN);
+  console.log('VALUE', intValue);
+  return intValue.toNumber();
 }
 async function deploy(folder) {
   const balances = await fetch(
@@ -13,7 +15,6 @@ async function deploy(folder) {
   )
     .then((res) => res.json())
     .then((result) => result.state.balances)
-    .then(map((v) => roundDown(v)))
     .then(
       omit([
         'vh-NTHVvlKZqRxc8LyyTNok65yQ55a_PJ1zWLb9G2JI',
@@ -22,55 +23,62 @@ async function deploy(folder) {
         '89tR0-C1m3_sCWCoVCChg4gFYKdiH5_ZDyZpdJ2DDRw',
         'uf_FqRvLqjnFMc8ZzGkF4qWKuNmUIQcYP0tPlCGORQk',
       ])
-    );
-  const jwk = JSON.parse(
-    fs.readFileSync(process.env.PATH_TO_WALLET).toString()
-  );
-  const warp = WarpFactory.forMainnet(defaultCacheOptions, true).use(
-    new DeployPlugin()
-  );
-  const contractSrc = fs.readFileSync(`${folder}/contract.js`, 'utf8');
-  const stateFromFile = JSON.parse(
-    fs.readFileSync(`${folder}/initial-state.json`, 'utf8')
-  );
-  if (!process.env.WALLET_ADDRESS) {
-    console.error(
-      'Set proces.env.WALLET_ADDRESS to your wallet addres. eg. 9x24zjvs9DA5zAz2DmqBWAg6XcxrrE-8w3EkpwRm4e4'
-    );
-    process.exit(1);
-  }
+    )
+    .then(map((v) => roundDown(v)));
 
-  const initialState = {
-    ...stateFromFile,
-    ...{
-      owner: process.env.WALLET_ADDRESS,
-    },
-  };
-
-  const deployed = await warp.deploy(
-    {
-      // wallet: new ArweaveSigner(jwk),
-      wallet: jwk,
-      initState: JSON.stringify({
-        ...initialState,
-        balances,
-      }),
-      src: contractSrc,
-      evaluationManifest: {
-        evaluationOptions: {
-          sourceType: SourceType.BOTH,
-          internalWrites: true,
-          allowBigInt: true,
-          unsafeClient: 'skip',
-        },
-      },
-    },
-    {
-      disabledBundling: true,
-    }
+  console.log(
+    'BALANCE',
+    balances['VYDyg7TPf5tB4wqeCOklGx5VWjrbRFkDnsCuFaWhtb8']
   );
-  console.log(`SEQ contractTxId ${deployed.contractTxId}`);
-  await waitForConfirmation(deployed.contractTxId);
+
+  // const jwk = JSON.parse(
+  //   fs.readFileSync(process.env.PATH_TO_WALLET).toString()
+  // );
+  // const warp = WarpFactory.forMainnet(defaultCacheOptions, true).use(
+  //   new DeployPlugin()
+  // );
+  // const contractSrc = fs.readFileSync(`${folder}/contract.js`, 'utf8');
+  // const stateFromFile = JSON.parse(
+  //   fs.readFileSync(`${folder}/initial-state.json`, 'utf8')
+  // );
+  // if (!process.env.WALLET_ADDRESS) {
+  //   console.error(
+  //     'Set proces.env.WALLET_ADDRESS to your wallet addres. eg. 9x24zjvs9DA5zAz2DmqBWAg6XcxrrE-8w3EkpwRm4e4'
+  //   );
+  //   process.exit(1);
+  // }
+
+  // const initialState = {
+  //   ...stateFromFile,
+  //   ...{
+  //     owner: process.env.WALLET_ADDRESS,
+  //   },
+  // };
+
+  // const deployed = await warp.deploy(
+  //   {
+  //     // wallet: new ArweaveSigner(jwk),
+  //     wallet: jwk,
+  //     initState: JSON.stringify({
+  //       ...initialState,
+  //       balances,
+  //     }),
+  //     src: contractSrc,
+  //     evaluationManifest: {
+  //       evaluationOptions: {
+  //         sourceType: SourceType.BOTH,
+  //         internalWrites: true,
+  //         allowBigInt: true,
+  //         unsafeClient: 'skip',
+  //       },
+  //     },
+  //   },
+  //   {
+  //     disabledBundling: true,
+  //   }
+  // );
+  // console.log(`SEQ contractTxId ${deployed.contractTxId}`);
+  // await waitForConfirmation(deployed.contractTxId);
 }
 deploy(process.argv[2]).catch(console.log);
 
