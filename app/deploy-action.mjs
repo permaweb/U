bcadca80f8be78c9103984d3047f7e00362af415
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-30 14:31:45 -0500

    ci: action script

--- app/deploy-action.mjs
+++ app/deploy-action.mjs
@@ -1,12 +1,6 @@
-import { readFileSync } from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import Bundlr from '@bundlr-network/client';
 import { WarpFactory, defaultCacheOptions } from 'warp-contracts';
 import Arweave from 'arweave';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const ANT = 'tudrGap6uMQ80zdFZsjAUkSp4Ea8YYsaKgskewqNWSU'; // rebar.arweave.dev
 const arweave = Arweave.init({
@@ -15,10 +9,6 @@ const arweave = Arweave.init({
   protocol: 'https',
 });
 
-const filePath = path.join(__dirname, 'contracts.json');
-const contracts = JSON.parse(readFileSync(filePath, 'utf8'));
-
-console.log('Contracts', contracts);
 // const jwk = JSON.parse(fs.readFileSync('../wallet.json', 'utf-8'))
 const jwk = JSON.parse(
   Buffer.from(process.env.WALLET, 'base64').toString('utf-8')
@@ -47,6 +37,4 @@ await contract.writeInteraction({
   transactionId: result.id,
 });
 
-console.log(
-  'Deployed Cookbook, please wait 20 - 30 minutes for ArNS to update!'
-);
+console.log('Deployed RebAR, please wait 20 - 30 minutes for ArNS to update!');
