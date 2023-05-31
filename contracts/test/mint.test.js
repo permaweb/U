744e19662eb8af61fd919bc51933f908cad750a4
Justin Shaw <mogulx_operates@yahoo.com>
2023-05-31 13:23:29 -0500

    test: remove 100 feron check

--- contracts/test/mint.test.js
+++ contracts/test/mint.test.js
@@ -32,32 +32,6 @@ test('should mint 1 rebar', async () => {
   assert.is(state.balances[caller], 1000000);
 });
 
-test('should throw You must mint at least 100 feron.', () => {
-  const caller = '<justin>';
-
-  const env = setupSmartWeaveEnv(
-    999999, // reward
-    0, // height
-    '<tx>'
-  );
-  const output = mint(
-    {
-      name: 'RebAR',
-      ticker: 'RebAR',
-      balances: {},
-      settings: [
-        ['communityLogo', '_32hAgwNt4ZVPisYAP3UQNUbwi_6LPUuZldPFCLm0fo'],
-        ['isTradeable', true],
-      ],
-      claimable: [],
-      divisibility: 1e6,
-    },
-    { caller }
-  );
-
-  const state = output.state;
-  assert.is(state.balances[caller], undefined);
-});
 test.after(async () => {});
 
 test.run();
