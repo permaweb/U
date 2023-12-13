2493b247004d060fdaed5635c7263252bf21bc14
Justin Shaw <mogulx_operates@yahoo.com>
2023-12-13 09:59:32 -0600

    fix(app): remove extra dre's

--- app/src/api/constants.ts
+++ app/src/api/constants.ts
@@ -1,9 +1,3 @@
 export const DRES = {
   'DRE-U': 'https://dre-u.warp.cc/contract',
-  'DRE-1': 'https://dre-1.warp.cc/contract',
-  'DRE-2': 'https://dre-2.warp.cc/contract',
-  'DRE-3': 'https://dre-3.warp.cc/contract',
-  'DRE-4': 'https://dre-4.warp.cc/contract',
-  'DRE-5': 'https://dre-5.warp.cc/contract',
-  'DRE-6': 'https://dre-6.warp.cc/contract',
 };
